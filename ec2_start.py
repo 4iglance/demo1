d87fbca36a177ab6ad575c9aa63f0e59dc813ab8
israrulhaque-at-847970100731 <hisrarul@gmail.com>
2020-04-29 06:19:30 +0530

    udated ec2_start.py

--- ec2_start.py
+++ ec2_start.py
@@ -1,3 +1,3 @@
 import boto3
 
-print("We are about to learn lambda function of AWS")
+print("We are about to learn lambda function of AWS, lambda is a whole new world. Uses docker or container in the bg")
